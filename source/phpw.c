7978a7aa3a7f606eef7ae66167b4859e93d6a3db
Bohuslav Šimek <bohuslav@simek.si>
2025-05-24 22:53:12 +0200

    first blood

--- source/phpw.c
+++ source/phpw.c
@@ -56,3 +56,38 @@ void EMSCRIPTEN_KEEPALIVE phpw_with_args(char *file, char *arg1, char *arg2)
   php_embed_shutdown();
   EMBED_SHUTDOWN = 1;
 }
+
+void EMSCRIPTEN_KEEPALIVE phpw_with_args_keepalive(char *file, char *arg1, char *arg2)
+{
+  if (EMBED_SHUTDOWN == 1) {
+    setenv("USE_ZEND_ALLOC", "0", 1);
+    php_embed_init(0, NULL);
+    EMBED_SHUTDOWN = 0;
+  }
+
+  zval args_array;
+  array_init(&args_array);
+  add_index_string(&args_array, 0, file);
+  add_index_string(&args_array, 1, arg1);
+  add_index_string(&args_array, 2, arg2);
+  zend_hash_str_update(&EG(symbol_table), "argv", strlen("argv"), &args_array);
+
+  zval argc_zval;
+  ZVAL_LONG(&argc_zval, 3);
+  zend_hash_str_update(&EG(symbol_table), "argc", strlen("argc"), &argc_zval);
+
+  zend_first_try {
+    zend_file_handle file_handle;
+    zend_stream_init_filename(&file_handle, file);
+
+    if (php_execute_script(&file_handle) == FAILURE) {
+      php_printf("Failed to execute PHP script.\n");
+    }
+
+    zend_destroy_file_handle(&file_handle);
+  } zend_catch {
+    /* int exit_status = EG(exit_status); */
+  } zend_end_try();
+
+  phpw_flush();
+}
